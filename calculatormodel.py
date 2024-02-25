31dd3598d8882c451d0ab717a965cfc003d9b1c9
Radek Tesař <radek.tesar135@gmail.com>
2024-02-25 22:15:54 +0100

    Přidání funkcí

--- calculatormodel.py
+++ calculatormodel.py
@@ -4,5 +4,18 @@ class CalculatorModel:
     def __init__(self):
         self.expression = ""
 
+    #Metoda sloužící k přidání uživatelského vstupu do výrazu
     def add_to_expression(self, value):
         self.expression += str(value)
+
+    #Vyhodnocení výrazu a aktualizace výsledku
+    def evaluate_expression(self):
+        try:
+            result = eval(self.expression)
+            self.expression = str(result)
+        except ZeroDivisionError:
+            self.expression = "Error: Division by zero"
+        except SyntaxError:
+            self.expression = "Error: Invalid syntax"
+        except Exception:
+            self.expression = "Error:" + str(e)
