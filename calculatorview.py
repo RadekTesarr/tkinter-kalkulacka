d32929207b4ea0361a500d3148fb84b7c813d840
Radek Tesař <radek.tesar135@gmail.com>
2024-02-25 22:39:43 +0100

    Přidání widgetu Entry a pozicování

--- calculatorview.py
+++ calculatorview.py
@@ -1,3 +1,9 @@
+import tkinter as tk
+
 class CalculatorView:
     def __int__(self, root, controller):
         self.controller == controller
+
+        self.entry = tk.Entry(root, width=20, font=("Arial", 14))
+        self.entry.grid(row=0, column=0, columnspan=5, padx=10, pady=10)
+
